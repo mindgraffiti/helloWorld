8fcac1e9bf52f97ce915a6e13da61155ec3a6f38
Thuy Copeland <Thuy@Shaunas-MacBook-Pro.local>
2013-03-14 13:45:44 -0500

    Initial Commit

---
+++ helloWorld/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  helloWorld
+//
+//  Created by Thuy Copeland on 3/14/13.
+//  Copyright (c) 2013 Thuy Copeland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ helloWorld/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  helloWorld
+//
+//  Created by Thuy Copeland on 3/14/13.
+//  Copyright (c) 2013 Thuy Copeland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
